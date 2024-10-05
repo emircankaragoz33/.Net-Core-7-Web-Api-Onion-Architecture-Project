03cb5417162aa6cd1737d66934b35c2efb5411c2
emircankaragoz33 <emircankaragoz@hotmail.com>
2024-10-05 13:44:48 +0300

    Development ve Production ortam değişkenleri ayarlandı.

---
+++ Prensentation/Project.Api/Program.cs
@@ -0,0 +1,28 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//degisken ortamlara erisimi buradan sagliyorum.
+var env = builder.Environment;
+//pathi verdim apinin.
+builder.Configuration.SetBasePath(env.ContentRootPath).AddJsonFile("appsettings.json",optional : false).AddJsonFile($"appsettings.{env.EnvironmentName}.json",optional : true);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
